6c5a50cc31adef0c226a316ee3959b41311cfa07
belmant <belmant@etud.insa-toulouse.fr>
2020-10-03 21:13:44 +0200

    Add one_level_outer_product

--- src/ConformalGeometry.jl
+++ src/ConformalGeometry.jl
@@ -1,5 +1,12 @@
 module ConformalGeometry
 
-# Write your package code here.
+function one_level_outer_product!(list, bit::Val{1})
+    push!(list, 0)
+    push!(list, 1)
+end
+
+function one_level_outer_product!(list, bit::Val{0})
+    push!(list, 0)
+end
 
 end
