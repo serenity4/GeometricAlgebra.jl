d3c0921a946780400f62d45a4dd5875badd94c92
serenity4 <cedric.bel@hotmail.fr>
2020-12-10 22:34:26 +0100

    Add more promotion tests

--- test/promotions.jl
+++ test/promotions.jl
@@ -1,4 +1,7 @@
 @testset "Promotions" begin
     @test 1v1 + 1.0v1 == 2.0v1
     @test 1v1 + 1.0v2 == 1.0v1 + 1.0v2
+    @test 1v1 - 1.0v2 == 1.0v1 - 1.0v2
+    @test 1v1 - 1.0v1 == 0.0v1
+    @test 1v1 ∧ 1.0v2 == 1v1 * 1.0v2 == 1.0v12
 end
