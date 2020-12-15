e40c2bde0194a2cacd7c0accac626676c5cd5840
serenity4 <cedric.bel@hotmail.fr>
2020-12-15 22:25:24 +0100

    Rework tests for geometric, inner and outer products

--- test/operators/geometric_product.jl
+++ test/operators/geometric_product.jl
@@ -1,20 +1,16 @@
 @testset "Geometric product" begin
-    @test v1   * v1         == 1v
-    @test v1   * v2         == 1v12
-    @test v2   * v2         == 1v
-    @test v3   * v3         == 1v
-    @test v1   * v12        == 1v2
-    @test 1v1  * 1v1        == 1v
-    @test 5v1  * 2v2        == 10v12
-    @test 1v2  * 1v1        == -1v12
-    @test 1v13 * 1v2        == -1v123
-    @test 1v1  * 1v2 * 1v12 == -1v
-    @test (5v1 + 3v3 + 1v12) * 5v2 == 25v12 - 15v23 + 5v1
-    @test 1v1 * 3v2         == -(3v2 * 1v1)
-    @test 1v1 * 3v23        == 3v23 * 1v1
     @test A * (B + C) ≈ A * B + A * C
     @test (B + C) * A ≈ B * A + C * A
     @test A + 𝟎 == A
     @test A * 1 == A
     @test A + (-A) ≈ zeros(MV)
+
+    for r ∈ 0:4
+        Aᵣ = As[r+1]
+        for s ∈ 0:4
+            Bₛ = Bs[s+1]
+            @test Aᵣ * Bₛ ≈ sum(map(k -> grade_projection(Aᵣ * Bₛ, abs(r - s) + 2k), 0:(r + s - abs(r - s)) ÷ 2))
+        end
+        @test a ⋅ Aᵣ ≈ 0.5 * (a * Aᵣ + (-1) ^ (r + 1) * Aᵣ * a)
+    end
 end
--- test/operators/outer_product.jl
+++ test/operators/outer_product.jl
@@ -1,16 +1,13 @@
 @testset "Outer product" begin
-    @test v1   ∧ v1         == 𝟎
-    @test v1   ∧ v2         == 1v12
-    @test v1   ∧ v12        == 𝟎
-    @test 1v1  ∧ 1v1        == 𝟎
-    @test 5v1  ∧ 2v2        == 10v12
-    @test 1v2  ∧ 1v1        == -1v12
-    @test 1v13 ∧ 1v2        == -1v123
-    @test 1v1  ∧ 1v2 ∧ 1v12 == 𝟎
-    @test ∧(v1, v3, v2)     == -1(v1 ∧ v2 ∧ v3)
+    @test A₃ ∧ 5 == 5 ∧ A₃ == 5A₃
+    @test ∧(v1, v3, v2) == -1(v1 ∧ v2 ∧ v3)
+
+    for r ∈ 0:4
+        Aᵣ = As[r+1]
+        @test a ∧ Aᵣ ≈ 0.5 * (a * Aᵣ + (-1) ^ r * Aᵣ * a)
+    end
+
     @test A ∧ (B + C) ≈ A ∧ B + A ∧ C
     @test (B + C) ∧ A ≈ B ∧ A + C ∧ A
-    @test A₁ ∧ B₂ ≈ 0.5 * (A₁ * B₂ + B₂ * A₁)
-    @test A₁ ∧ B₃ ≈ 0.5 * (A₁ * B₃ - B₃ * A₁)
 end
 
