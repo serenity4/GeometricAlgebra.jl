566b0fe60af9b62e2f456a8aead617867e8e577e
serenity4 <cedric.bel@hotmail.fr>
2020-12-15 22:26:09 +0100

    Improve grade projection tests

--- test/operators/projections.jl
+++ test/operators/projections.jl
@@ -2,7 +2,9 @@
     @testset "Projection to grade $r" for r ∈ 0:4 begin
             Pᵣ = x -> grade_projection(x, r)
             Aᵣ, Bᵣ = As[r+1], Bs[r+1]
-            @test Pᵣ(D) == Bs[r+1]
+            @test Pᵣ(SA) ≈ As[r+1]
+            @test Pᵣ(SB) ≈ Bs[r+1]
+            @test Pᵣ(SC) ≈ Cs[r+1]
             @test Pᵣ(A * B) ≈ (-1) ^ (r * (r - 1) ÷ 2) * Pᵣ(reverse(A * B))
             @test Pᵣ(Aᵣ * B₁) ≈ Pᵣ(reverse(B₁) * Aᵣ) ≈ Pᵣ(B₁ * Aᵣ)
             @test Pᵣ(Aᵣ * B₂) ≈ Pᵣ(reverse(B₂) * Aᵣ) ≈ -Pᵣ(B₂ * Aᵣ)
