cda514e457c5039f507129a8ef8d615fbafd4107
serenity4 <cedric.bel@hotmail.fr>
2020-12-16 16:09:48 +0100

    Test all possibilities

--- test/operators/projections.jl
+++ test/operators/projections.jl
@@ -6,9 +6,11 @@
             @test Pᵣ(SB) ≈ Bs[r+1]
             @test Pᵣ(SC) ≈ Cs[r+1]
             @test Pᵣ(A * B) ≈ (-1) ^ (r * (r - 1) ÷ 2) * Pᵣ(reverse(A * B))
-            @test Pᵣ(Aᵣ * B₁) ≈ Pᵣ(reverse(B₁) * Aᵣ) ≈ Pᵣ(B₁ * Aᵣ)
-            @test Pᵣ(Aᵣ * B₂) ≈ Pᵣ(reverse(B₂) * Aᵣ) ≈ -Pᵣ(B₂ * Aᵣ)
             @test Pᵣ(A * Bᵣ * C) ≈ Pᵣ(reverse(C) * Bᵣ * reverse(A))
+            for s ∈ 0:4
+                Bₛ = Bs[s+1]
+                @test Pᵣ(Aᵣ * Bₛ) ≈ Pᵣ(reverse(Bₛ) * Aᵣ) ≈ (-1) ^ (s * (s - 1) ÷ 2) * Pᵣ(Bₛ * Aᵣ)
+            end
         end
     end
 end
