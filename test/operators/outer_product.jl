57b50866ed5df81adeee8bf5b55586281768622a
serenity4 <cedric.bel@hotmail.fr>
2020-12-21 22:07:54 +0100

    Improve test

--- test/operators/outer_product.jl
+++ test/operators/outer_product.jl
@@ -1,6 +1,6 @@
 @testset "Outer product" begin
     @test A₃ ∧ 5 == 5 ∧ A₃ == 5A₃
-    @test ∧(v1, v3, v2) == -1(v1 ∧ v2 ∧ v3)
+    @test ∧(v1, v3, v2) == -1(v1 ∧ v2 ∧ v3) == -1v123
 
     for r ∈ 0:4
         Aᵣ = As[r+1]
