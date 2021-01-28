f79aecb7df50389befa78539623e73e153339807
serenity4 <cedric.bel@hotmail.fr>
2021-01-28 12:15:26 +0100

    Use v4 as e in 3D CGA

--- test/algebras/3d_cga.jl
+++ test/algebras/3d_cga.jl
@@ -2,8 +2,8 @@ using GeometricAlgebra
 
 @basis "++++-"
 
-const n = 1.0v1 + 1.0v5
-const n̄ = 1.0v1 - 1.0v5
+const n = 1.0v4 + 1.0v5
+const n̄ = 1.0v4 - 1.0v5
 
 point(x) = 0.5 * (2x + magnitude2(x) * n - n̄)
 point_pair(A, B) = A ∧ B
@@ -31,8 +31,8 @@ center(X) = X * n * X
     @testset "Conformal points" begin
         @test O == -0.5n̄
 
-        a = sum(randn(3) .* [v2, v3, v4])
-        b = sum(randn(3) .* [v2, v3, v4])
+        a = sum(randn(3) .* [v1, v2, v3])
+        b = sum(randn(3) .* [v1, v2, v3])
 
         A = point(a)
         B = point(b)
@@ -42,25 +42,25 @@ center(X) = X * n * X
     end
 
     @testset "Circles" begin
-        C1 = circle(point(1v2), point(1v3), point(-1v2))
-        @test C1 == 2.0v235
+        C1 = circle(point(1v1), point(1v2), point(-1v1))
+        @test C1 == 2.0v125
         @test normalize(center(C1)) == O
         @test radius2(C1) == 1v
 
-        C2 = circle(point(4v2), point(2v2 + 2v3), point(0v1))
-        @test C2 == 4(-v123 + v235 + 4v135)
-        @test normalize(center(C2)) == point(2v2)
+        C2 = circle(point(4v1), point(2v1 + 2v2), point(0v1))
+        @test C2 == 4(-v124 + v125 - 4v245)
+        @test normalize(center(C2)) == point(2v1)
         @test radius2(C2) == 4v
     end
 
     @testset "Spheres" begin
-        S1 = sphere(point(1v2), point(1v3), point(1v4), point(-1v2))
-        @test S1 == 2.0v2345
+        S1 = sphere(point(1v1), point(1v2), point(1v3), point(-1v1))
+        @test S1 == 2.0v1235
         @test normalize(center(S1)) == O
         @test radius2(S1) == 1v
 
-        S2 = sphere(point(0v1), point(4v2), point(2v2 + 2v3), point(2v2 + 2v4))
-        @test normalize(center(S2)) == point(2v2)
+        S2 = sphere(point(0v1), point(4v1), point(2v1 + 2v2), point(2v1 + 2v3))
+        @test normalize(center(S2)) == point(2v1)
         @test radius2(S2) == 4v
     end
 end
